207a784186e4788386a188c9efed87dba02e4a22
sreedhar333 <gsreedhar420@gmail.com>
2018-10-08 08:50:59 +0530

    Controller

--- SpringMVC/MvcSampleProject/src/com/hello/controllers/HelloController.java
+++ SpringMVC/MvcSampleProject/src/com/hello/controllers/HelloController.java
@@ -10,12 +10,15 @@ import org.springframework.ui.ModelMap;
 @Controller
 public class HelloController{
 	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String welcomePage(ModelMap model) {
+		return "redirect:/hello";
+	}
+	
    @RequestMapping(value="/hello", method = RequestMethod.GET)
    public String printHello(ModelMap model) 
    {
       model.addAttribute("message", "Hello Spring MVC Framework! Unique hire");
       return "hello";
    }
-   
-   
 }
