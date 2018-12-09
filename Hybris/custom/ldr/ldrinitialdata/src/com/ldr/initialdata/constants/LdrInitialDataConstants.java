49099b6cf58be87e4d4b86f1fa3b492b17689dc3
DevendraReddy <devendrareddyinfo@gmail.com>
2018-12-09 08:25:47 +0530

    Initial Commit

---
+++ Hybris/custom/ldr/ldrinitialdata/src/com/ldr/initialdata/constants/LdrInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.ldr.initialdata.constants;
+
+/**
+ * Global class for all LdrInitialData constants.
+ */
+public final class LdrInitialDataConstants extends GeneratedLdrInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "ldrinitialdata";
+
+	private LdrInitialDataConstants()
+	{
+		//empty
+	}
+}
